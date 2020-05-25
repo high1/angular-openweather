0b9c1ea58df125c27289284ed24f16f48fbc7f74
Radomir Simic <r.simic@levi9.com>
2020-05-25 22:59:05 +0200

    Update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '47efc2bd7074b85a1f972e428ca35469',
+  apiUrl: 'https://api.openweathermap.org/data/2.5',
+  apiInterval: 5 * 60 * 1000, // in minutes
+  cities: [
+      // Amsterdam
+      2759794,
+      // London
+      2643743,
+      // Helsinki
+      658225,
+      // Madrid
+      3117735,
+      // Moscow
+      524901
+  ]
 };
